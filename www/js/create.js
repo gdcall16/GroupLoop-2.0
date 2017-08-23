e9baad8c586069d374aaa82d502ea76a5db88bf7
Jenna Kaufman <jnkaufman@live.com>
2017-08-23 14:28:43 -0700

    Will it work??????????????

--- www/js/create.js
+++ www/js/create.js
@@ -8,8 +8,11 @@ var config = {
   messagingSenderId: "130341591298"
 };
 firebase.initializeApp(config);
+//create database called database
 var database = firebase.database();
 var currentUser = "";
+
+sessionStorage.setItem("username", "Gabi");
 
 function start(){
   var username = sessionStorage.username;
@@ -27,27 +30,30 @@ function numGen() {
   //if/else code to look through database and see if the pin has been used before
   return pin;
 }
-
 //Store the data of the project
 function storeData(){
-  var className = document.getElementById('classname').value;
-  var projName = document.getElementById('name').value;
+  var className = document.getElementById('name').value;
+  var projName = document.getElementById('email').value;
   var dateDue = document.getElementById('duedate').value;
   //Generate project id
   var projId = numGen();
+  sessionStorage.setItem("id", projId);
   //print project id on screen and replace contents of screen
   document.getElementById('create').style = "display:none;";
   var pin = document.createElement('h3');
   pin.textContent = "Your project id is: " + projId;
-  var z = document.getElementById("text");
-  z.appendChild(pin);
+  document.body.appendChild(pin);
+  var link = document.createElement('a');
+  link.href = "project.html";
+  link.innerHTML = "Go to project";
+  document.body.appendChild(link);
   //Add project data to the database
-  sessionStorage.setItem('id', projId);
   firebase.database().ref("groups/" + projId).set({
     //TODO add user to list of users
     name: projName,
     duedate: dateDue,
     classname: className,
+    summary: "",
   });
   firebase.database().ref("groups/" + projId + "/users/" + currentUser).set({
     task0: true,
